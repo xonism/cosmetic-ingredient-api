9a448aa74f4d830239aaa855f9e0fd828bc68066
ZENITECH\gabriele.mincyte <gabriele.mincyte@zenitech.co.uk>
2023-03-04 23:41:41 +0200

    Add existsByName method to IngredientRepository

--- src/main/java/cosmeticingredientapi/repositories/IngredientRepository.java
+++ src/main/java/cosmeticingredientapi/repositories/IngredientRepository.java
@@ -2,6 +2,9 @@ package cosmeticingredientapi.repositories;
 
 import cosmeticingredientapi.models.Ingredient;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface IngredientRepository extends JpaRepository<Ingredient, Long> {
+    boolean existsByName(String name);
 }
