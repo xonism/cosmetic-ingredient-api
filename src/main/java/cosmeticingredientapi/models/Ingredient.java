bd792ac9eec412908539eec97ac8235059ea6441
ZENITECH\gabriele.mincyte <gabriele.mincyte@zenitech.co.uk>
2023-03-04 23:41:18 +0200

    Make ingredient name unique

--- src/main/java/cosmeticingredientapi/models/Ingredient.java
+++ src/main/java/cosmeticingredientapi/models/Ingredient.java
@@ -18,7 +18,7 @@ public class Ingredient {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private String name;
 
     @Column(name = "safety_level_id", nullable = false)
