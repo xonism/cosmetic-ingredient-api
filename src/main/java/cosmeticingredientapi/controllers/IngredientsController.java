37e6583611bb682c770473cb4c764aad78f7a4bc
ZENITECH\gabriele.mincyte <gabriele.mincyte@zenitech.co.uk>
2023-03-04 21:33:01 +0200

    Add Ingredients & SafetyLevels controllers

--- src/main/java/cosmeticingredientapi/controllers/IngredientsController.java
+++ src/main/java/cosmeticingredientapi/controllers/IngredientsController.java
@@ -1,61 +1,24 @@
 package cosmeticingredientapi.controllers;
 
-import cosmeticingredientapi.enums.Ingredient;
-import cosmeticingredientapi.records.IngredientInfo;
-import cosmeticingredientapi.records.IngredientsRequest;
+import cosmeticingredientapi.models.Ingredient;
+import cosmeticingredientapi.repositories.IngredientRepository;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/v1/ingredients")
 public class IngredientsController {
-    @GetMapping("/ingredients")
-    public List<IngredientInfo> getIngredients() {
-        return convertIngridientEnumValuesToIngredientInfoList();
+    private final IngredientRepository ingredientsRepository;
+
+    public IngredientsController(IngredientRepository ingredientsRepository) {
+        this.ingredientsRepository = ingredientsRepository;
     }
 
-    public List<IngredientInfo> convertIngridientEnumValuesToIngredientInfoList() {
-        return Arrays.stream(Ingredient.values())
-                .map(ingredient -> new IngredientInfo(
-                        ingredient.getId(),
-                        ingredient.getName(),
-                        ingredient.getSafetyLevel().getValue()))
-                .toList();
-    }
-
-    @PostMapping("/ingredients")
-    public List<IngredientInfo> postIngredients(@RequestBody IngredientsRequest ingredientsRequest) {
-        return getIngredientsWithSafetyLevel(ingredientsRequest);
-    }
-
-    public List<IngredientInfo> getIngredientsWithSafetyLevel(IngredientsRequest ingredientsRequest) {
-        List<String> ingredientEnumNames = Ingredient.getAllIngredientEnumNames();
-
-        List<String> requestIngredientNameList =
-                Arrays.stream(getFormattedIngredientsString(ingredientsRequest).split(","))
-                        .map(ingredientName -> ingredientName.trim().toLowerCase())
-                        .toList();
-
-        return requestIngredientNameList.stream()
-                .map(requestIngredientName -> {
-                    if (!ingredientEnumNames.contains(requestIngredientName)) {
-                        return IngredientInfo.getObjectWithNullIdAndSafetyLevel(requestIngredientName);
-                    }
-                    Ingredient ingredient =
-                            Ingredient.valueOf(requestIngredientName.toUpperCase().replace(" ", "_"));
-                    return new IngredientInfo(
-                            ingredient.getId(),
-                            ingredient.getName(),
-                            ingredient.getSafetyLevel().getValue());
-                })
-                .toList();
-    }
-
-    private String getFormattedIngredientsString(IngredientsRequest ingredientsRequest) {
-        return ingredientsRequest.ingredients().replace("*", "");
+    @GetMapping
+    public List<Ingredient> getAllIngredients() {
+        return ingredientsRepository.findAll();
     }
 }
---
+++ src/main/java/cosmeticingredientapi/controllers/SafetyLevelsController.java
@@ -0,0 +1,24 @@
+package cosmeticingredientapi.controllers;
+
+import cosmeticingredientapi.models.SafetyLevel;
+import cosmeticingredientapi.repositories.SafetyLevelRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/safety-levels")
+public class SafetyLevelsController {
+    private final SafetyLevelRepository safetyLevelRepository;
+
+    public SafetyLevelsController(SafetyLevelRepository safetyLevelRepository) {
+        this.safetyLevelRepository = safetyLevelRepository;
+    }
+
+    @GetMapping
+    public List<SafetyLevel> getAllSafetyLevels() {
+        return safetyLevelRepository.findAll();
+    }
+}
