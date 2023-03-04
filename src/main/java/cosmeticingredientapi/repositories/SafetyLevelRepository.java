f881a209ec5712a76b26451e154cb686d0722473
ZENITECH\gabriele.mincyte <gabriele.mincyte@zenitech.co.uk>
2023-03-04 23:46:44 +0200

    Add @Repository annotation

--- src/main/java/cosmeticingredientapi/repositories/SafetyLevelRepository.java
+++ src/main/java/cosmeticingredientapi/repositories/SafetyLevelRepository.java
@@ -2,6 +2,8 @@ package cosmeticingredientapi.repositories;
 
 import cosmeticingredientapi.models.SafetyLevel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SafetyLevelRepository extends JpaRepository<SafetyLevel, Long> {
 }
