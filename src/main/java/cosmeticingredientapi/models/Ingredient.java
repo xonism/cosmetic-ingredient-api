fc365e3ddd018fb63e825c5de2dd5a164d136ea9
ZENITECH\gabriele.mincyte <gabriele.mincyte@zenitech.co.uk>
2023-03-04 21:30:35 +0200

    Add Ingredient & SafetyLevel models

---
+++ src/main/java/cosmeticingredientapi/models/Ingredient.java
@@ -0,0 +1,26 @@
+package cosmeticingredientapi.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "ingredient")
+public class Ingredient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(name = "safety_level_id", nullable = false)
+    private int safetyLevelId;
+}
