dd063fae8819f245303fb02e5fc1acd7c2aba64e
ZENITECH\gabriele.mincyte <gabriele.mincyte@zenitech.co.uk>
2023-03-20 23:56:33 +0200

    Remove error prevention from IngredientsService

--- src/main/java/cosmeticingredientapi/services/IngredientsService.java
+++ src/main/java/cosmeticingredientapi/services/IngredientsService.java
@@ -1,9 +1,7 @@
 package cosmeticingredientapi.services;
 
 import cosmeticingredientapi.models.Ingredient;
-import cosmeticingredientapi.records.Error;
 import cosmeticingredientapi.repositories.IngredientRepository;
-import cosmeticingredientapi.repositories.SafetyLevelRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -13,11 +11,9 @@ import java.util.List;
 @Service
 public class IngredientsService {
     private final IngredientRepository ingredientsRepository;
-    private final SafetyLevelRepository safetyLevelRepository;
 
-    public IngredientsService(IngredientRepository ingredientsRepository, SafetyLevelRepository safetyLevelRepository) {
+    public IngredientsService(IngredientRepository ingredientsRepository) {
         this.ingredientsRepository = ingredientsRepository;
-        this.safetyLevelRepository = safetyLevelRepository;
     }
 
     public ResponseEntity<List<Ingredient>> getAllIngredients() {
@@ -29,23 +25,6 @@ public class IngredientsService {
 
     public ResponseEntity<Object> createIngredient(Ingredient ingredient) {
         ingredient.setName(ingredient.getName().trim().toLowerCase());
-
-        boolean doesIngredientExist = ingredientsRepository.existsByName(ingredient.getName());
-        if (doesIngredientExist) {
-            return new ResponseEntity<>(
-                    new Error(String.format("Ingredient '%s' already exists", ingredient.getName())),
-                    HttpStatus.BAD_REQUEST
-            );
-        }
-
-        boolean doesSafetyLevelIdExist = safetyLevelRepository.findById(ingredient.getSafetyLevelId()).isPresent();
-        if (!doesSafetyLevelIdExist) {
-            return new ResponseEntity<>(
-                    new Error(String.format("Safety level ID '%s' does not exist", ingredient.getSafetyLevelId())),
-                    HttpStatus.BAD_REQUEST
-            );
-        }
-
         return new ResponseEntity<>(
                 ingredientsRepository.save(ingredient),
                 HttpStatus.CREATED
