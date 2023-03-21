579bad00c7898d925d9b503860a6f09ef02de454
ZENITECH\gabriele.mincyte <gabriele.mincyte@zenitech.co.uk>
2023-03-21 10:07:56 +0200

    Add ManyToOne relationship

--- src/main/java/cosmeticingredientapi/models/Ingredient.java
+++ src/main/java/cosmeticingredientapi/models/Ingredient.java
@@ -1,10 +1,13 @@
 package cosmeticingredientapi.models;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
@@ -21,6 +24,7 @@ public class Ingredient {
     @Column(nullable = false, unique = true)
     private String name;
 
-    @Column(name = "safety_level_id", nullable = false)
-    private int safetyLevelId;
+    @ManyToOne(cascade = CascadeType.MERGE)
+    @JoinColumn(name = "safety_level_id", referencedColumnName = "id")
+    private SafetyLevel safetyLevel;
 }
