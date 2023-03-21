b411e797dbb6edfde6dbf68dfe7aa0a557266763
ZENITECH\gabriele.mincyte <gabriele.mincyte@zenitech.co.uk>
2023-03-21 10:11:18 +0200

    Remove old files

--- src/main/java/cosmeticingredientapi/records/IngredientInfo.java
+++
@@ -1,7 +0,0 @@
-package cosmeticingredientapi.records;
-
-public record IngredientInfo (Long id, String name, String safetyLevel) {
-    public static IngredientInfo getObjectWithNullIdAndSafetyLevel(String ingredientName) {
-        return new IngredientInfo(null, ingredientName, null);
-    }
-}
--- src/main/java/cosmeticingredientapi/records/IngredientsRequest.java
+++
@@ -1,4 +0,0 @@
-package cosmeticingredientapi.records;
-
-public record IngredientsRequest(String ingredients) {
-}
