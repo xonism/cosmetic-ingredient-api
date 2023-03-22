71ae85a13f83c6bae4d7905948f2de2ca0bc2989
ZENITECH\gabriele.mincyte <gabriele.mincyte@zenitech.co.uk>
2023-03-22 21:31:18 +0200

    Add createSafetyLevel

--- src/main/java/cosmeticingredientapi/controllers/SafetyLevelsController.java
+++ src/main/java/cosmeticingredientapi/controllers/SafetyLevelsController.java
@@ -1,10 +1,13 @@
 package cosmeticingredientapi.controllers;
 
 import cosmeticingredientapi.models.SafetyLevel;
+import cosmeticingredientapi.records.SafetyLevelRequest;
 import cosmeticingredientapi.services.SafetyLevelsService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,5 +30,10 @@ public class SafetyLevelsController {
     @GetMapping(value = "/{id}")
     public ResponseEntity<Object> getSafetyLevel(@PathVariable Long id) {
         return safetyLevelsService.getSafetyLevel(id);
+    }
+
+    @PostMapping
+    public ResponseEntity<Object> createSafetyLevel(@RequestBody SafetyLevelRequest safetyLevelRequest) {
+        return safetyLevelsService.createSafetyLevel(safetyLevelRequest);
     }
 }
