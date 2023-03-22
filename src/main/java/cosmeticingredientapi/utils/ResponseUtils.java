02f7e7ee6bce87929b8d81b9ae1e3d01dcc0be58
ZENITECH\gabriele.mincyte <gabriele.mincyte@zenitech.co.uk>
2023-03-22 23:04:15 +0200

    Remove word Entity from method names in ResponseUtils

--- src/main/java/cosmeticingredientapi/utils/ResponseUtils.java
+++ src/main/java/cosmeticingredientapi/utils/ResponseUtils.java
@@ -9,14 +9,14 @@ public class ResponseUtils {
 
     }
 
-    public static ResponseEntity<Object> createNotFoundByIdErrorResponseEntity(String entityName, Long id) {
+    public static ResponseEntity<Object> createNotFoundByIdErrorResponse(String entityName, Long id) {
         String message = String.format("%s with ID %s not found", entityName, id);
         return new ResponseEntity<>(
                 new Error(message, TimeUtils.getTimestamp()),
-                HttpStatus.BAD_REQUEST);
+                HttpStatus.NOT_FOUND);
     }
 
-    public static ResponseEntity<Object> createMustNotBeNullResponseEntity(String propertyName) {
+    public static ResponseEntity<Object> createMustNotBeNullResponse(String propertyName) {
         String message = String.format("%s must not be null", propertyName);
         return new ResponseEntity<>(
                 new Error(message, TimeUtils.getTimestamp()),
