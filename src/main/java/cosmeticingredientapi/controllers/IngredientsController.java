744174fba5947dfeab5a4b55c22ce54b04a425b7
ZENITECH\gabriele.mincyte <gabriele.mincyte@zenitech.co.uk>
2023-03-22 20:56:54 +0200

    Add getIngredient

--- src/main/java/cosmeticingredientapi/controllers/IngredientsController.java
+++ src/main/java/cosmeticingredientapi/controllers/IngredientsController.java
@@ -5,6 +5,7 @@ import cosmeticingredientapi.records.IngredientRequest;
 import cosmeticingredientapi.services.IngredientsService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,6 +25,11 @@ public class IngredientsController {
     @GetMapping
     public ResponseEntity<List<Ingredient>> getAllIngredients() {
         return ingredientsService.getAllIngredients();
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Object> getIngredient(@PathVariable Long id) {
+        return ingredientsService.getIngredient(id);
     }
 
     @PostMapping
