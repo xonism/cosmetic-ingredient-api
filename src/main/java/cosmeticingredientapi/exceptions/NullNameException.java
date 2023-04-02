bcb32db6c9a6b8a80b266edd48f7f6c69bc028b6
ZENITECH\gabriele.mincyte <gabriele.mincyte@zenitech.co.uk>
2023-04-02 22:10:01 +0300

    Remove NullNameException.java

--- src/main/java/cosmeticingredientapi/exceptions/NullNameException.java
+++
@@ -1,7 +0,0 @@
-package cosmeticingredientapi.exceptions;
-
-public class NullNameException extends RuntimeException {
-    public NullNameException(String entityName) {
-        super(String.format("%s name must not be null", entityName));
-    }
-}
