d9288df0379bbec076a28b4d7a21a1110b256740
ZENITECH\gabriele.mincyte <gabriele.mincyte@zenitech.co.uk>
2023-04-02 22:10:40 +0300

    Add an exception handler for IdMismatchException

--- src/main/java/cosmeticingredientapi/exceptions/RestExceptionHandler.java
+++ src/main/java/cosmeticingredientapi/exceptions/RestExceptionHandler.java
@@ -7,6 +7,7 @@ import jakarta.validation.ConstraintViolation;
 import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -45,9 +46,9 @@ public class RestExceptionHandler {
                 HttpStatus.BAD_REQUEST);
     }
 
-    @ExceptionHandler(value = {NullNameException.class})
-    public ResponseEntity<Error> handleNullValueException(
-            NullNameException exception
+    @ExceptionHandler(value = {IdMismatchException.class})
+    public ResponseEntity<Error> handleIdMismatchException(
+            IdMismatchException exception
     ) {
         return new ResponseEntity<>(
                 new Error(exception.getMessage(), TimeUtils.getTimestamp()),
