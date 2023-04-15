419f1f383e501ea14b0076008c8fdaa812a5ce24
ZENITECH\gabriele.mincyte <gabriele.mincyte@zenitech.co.uk>
2023-04-15 22:17:22 +0300

    Add getIngredientsWithSafetyLevels endpoint

--- src/main/java/cosmeticingredientapi/controllers/IngredientsController.java
+++ src/main/java/cosmeticingredientapi/controllers/IngredientsController.java
@@ -1,12 +1,15 @@
 package cosmeticingredientapi.controllers;
 
 import cosmeticingredientapi.models.Ingredient;
+import cosmeticingredientapi.records.IngredientRequest;
 import cosmeticingredientapi.services.IngredientsService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -35,6 +38,13 @@ public class IngredientsController {
     public ResponseEntity<Ingredient> getIngredient(@PathVariable Long id) {
         return new ResponseEntity<>(
                 ingredientsService.getIngredientById(id),
+                HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<List<Ingredient>> getIngredientsWithSafetyLevels(@RequestBody IngredientRequest ingredientRequest) {
+        return new ResponseEntity<>(
+                ingredientsService.getIngredientsWithSafetyLevels(ingredientRequest),
                 HttpStatus.OK);
     }
 }
