73ae1be95e7643e5899c84357ab17521569ab01e
ZENITECH\gabriele.mincyte <gabriele.mincyte@zenitech.co.uk>
2023-04-15 22:16:21 +0300

    Add findByName to SafetyLevelRepository

--- src/main/java/cosmeticingredientapi/repositories/SafetyLevelRepository.java
+++ src/main/java/cosmeticingredientapi/repositories/SafetyLevelRepository.java
@@ -6,4 +6,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SafetyLevelRepository extends JpaRepository<SafetyLevel, Long> {
+
+    SafetyLevel findByName(String name);
 }
