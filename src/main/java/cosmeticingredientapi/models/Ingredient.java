6de5a4e3122b0972ddf2da6fbedcf538b33aec1c
ZENITECH\gabriele.mincyte <gabriele.mincyte@zenitech.co.uk>
2023-06-15 09:03:04 +0300

    Remove UNSIGNED definition in Ingredient model

--- src/main/java/cosmeticingredientapi/models/Ingredient.java
+++ src/main/java/cosmeticingredientapi/models/Ingredient.java
@@ -28,6 +28,6 @@ public class Ingredient {
     private String name;
 
     @ManyToOne(cascade = CascadeType.MERGE)
-    @JoinColumn(name = "safety_level_id", referencedColumnName = "id", columnDefinition="UNSIGNED")
+    @JoinColumn(name = "safety_level_id", referencedColumnName = "id")
     private SafetyLevel safetyLevel;
 }
