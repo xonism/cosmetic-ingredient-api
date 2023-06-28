5daf84ffe1eba71fe051ac32fc6a629ead2a7ff0
ZENITECH\gabriele.mincyte <gabriele.mincyte@zenitech.co.uk>
2023-06-28 09:27:14 +0300

    Add User entity

---
+++ src/main/java/cosmeticingredientapi/models/User.java
@@ -0,0 +1,38 @@
+package cosmeticingredientapi.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    private long id;
+
+    @NotBlank
+    @Column(unique = true, nullable = false)
+    private String login;
+
+    @NotBlank
+    @Column(nullable = false)
+    private String location;
+
+    @NotBlank
+    @Column(nullable = false)
+    private String role;
+
+    @NotBlank
+    @Column(name = "avatar_url", unique = true, nullable = false)
+    private String avatarUrl;
+
+    @Column(name = "two_factor_authentication", nullable = false)
+    private String twoFactorAuthentication;
+}
